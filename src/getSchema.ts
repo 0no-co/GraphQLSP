0f9ce77e2883bd59b24d655496f0520074f3c338
jdecroock <decroockjovi@gmail.com>
2023-04-14 20:39:39 +0200

    resolve introspection from url

--- src/getSchema.ts
+++ src/getSchema.ts
@@ -1,26 +1,82 @@
-import { GraphQLSchema, buildSchema, buildClientSchema } from 'graphql';
+import {
+  GraphQLSchema,
+  buildSchema,
+  buildClientSchema,
+  getIntrospectionQuery,
+  IntrospectionQuery,
+} from 'graphql';
+import fetch from 'node-fetch';
 import path from 'path';
 import fs from 'fs';
 
+import { Logger } from './index';
+
 export const loadSchema = (
   root: string,
-  schema: string
+  schema: string,
+  logger: Logger
 ): { current: GraphQLSchema | null } => {
   const ref: { current: GraphQLSchema | null } = { current: null };
-  const isJson = schema.endsWith('json');
-  const resolvedPath = path.resolve(path.dirname(root), schema);
-  const contents = fs.readFileSync(resolvedPath, 'utf-8');
+  let url: URL | undefined;
 
-  fs.watchFile(resolvedPath, () => {
+  try {
+    url = new URL(schema);
+  } catch (e) {}
+
+  if (url) {
+    logger(`Fetching introspection from ${url.toString()}`);
+    fetch(url.toString(), {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: getIntrospectionQuery({
+          descriptions: true,
+          schemaDescription: true,
+          inputValueDeprecation: false,
+          directiveIsRepeatable: false,
+          specifiedByUrl: false,
+        }),
+      }),
+    })
+      .then(response => {
+        logger(`Got response ${response.statusText} ${response.status}`);
+        if (response.ok) return response.json();
+        else return response.text();
+      })
+      .then(result => {
+        if (typeof result === 'string') {
+          logger(`Got error while fetching introspection ${result}`);
+        } else {
+          try {
+            ref.current = buildClientSchema(
+              (result as { data: IntrospectionQuery }).data
+            );
+            logger(`Got schema for ${url!.toString()}`);
+          } catch (e: any) {
+            logger(`Got schema error for ${e.message}`);
+          }
+        }
+      });
+  } else {
+    const isJson = schema.endsWith('json');
+    const resolvedPath = path.resolve(path.dirname(root), schema);
+    logger(`Getting schema from ${resolvedPath}`);
     const contents = fs.readFileSync(resolvedPath, 'utf-8');
+
+    fs.watchFile(resolvedPath, () => {
+      const contents = fs.readFileSync(resolvedPath, 'utf-8');
+      ref.current = isJson
+        ? buildClientSchema(JSON.parse(contents))
+        : buildSchema(contents);
+    });
+
     ref.current = isJson
       ? buildClientSchema(JSON.parse(contents))
       : buildSchema(contents);
-  });
-
-  ref.current = isJson
-    ? buildClientSchema(JSON.parse(contents))
-    : buildSchema(contents);
+    logger(`Got schema and initialized watcher for ${schema}`);
+  }
 
   return ref;
 };

d702223f57f750ba9a5a461855fb5afb2a129ea5
Daniel <daniellehr@gmx.de>
2023-04-16 15:36:26 +0200

    Add dev watch build (#29)
    * Improve DX
    
    * Add launch script and add port to launch.json
    
    * Fixes after switch to monorepo

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,30 +1,34 @@
 import typescript from '@rollup/plugin-typescript';
 import terser from '@rollup/plugin-terser';
 
+const isDev = process.env.NODE_ENV === 'development';
+
+/** @type {import('rollup').RollupOptions} */
 export default {
   input: './src/index.ts',
   output: [
     { file: './dist/index.module.js', format: 'esm' },
-    { file: './dist/index.js', format: 'cjs' }
+    { file: './dist/index.js', format: 'cjs' },
   ],
   plugins: [
     typescript(),
-    terser({
-      warnings: true,
-      ecma: 2015,
-      ie8: false,
-      toplevel: true,
-      compress: {
-        keep_infinity: true,
-        pure_getters: true,
-        passes: 10
-      },
-      mangle: {
-        module: true,
-      },
-      output: {
-        comments: false
-      }
-    })
-  ]
+    !isDev &&
+      terser({
+        warnings: true,
+        ecma: 2015,
+        ie8: false,
+        toplevel: true,
+        compress: {
+          keep_infinity: true,
+          pure_getters: true,
+          passes: 10,
+        },
+        mangle: {
+          module: true,
+        },
+        output: {
+          comments: false,
+        },
+      }),
+  ],
 };
---
+++ scripts/launch-debug.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+# Check if code is in PATH
+
+if ! command -v code &> /dev/null
+then
+    echo "Make sure to add VS Code to your PATH:"
+    echo "https://code.visualstudio.com/docs/setup/mac#_launching-from-the-command-line"
+    exit
+fi
+
+TSS_DEBUG=9559 code --user-data-dir ~/.vscode-debug/ packages/example

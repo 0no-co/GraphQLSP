096fad799f266a19173abd7e5f7cb85b74f5baa8
jdecroock <decroockjovi@gmail.com>
2023-04-20 13:29:31 +0200

    refactor autocomplete

---
+++ packages/graphqlsp/src/autoComplete.ts
@@ -0,0 +1,113 @@
+import ts from 'typescript/lib/tsserverlibrary';
+import {
+  ScriptElementKind,
+  isIdentifier,
+  isNoSubstitutionTemplateLiteral,
+  isTaggedTemplateExpression,
+  isTemplateExpression,
+  isToken,
+} from 'typescript';
+import {
+  getAutocompleteSuggestions,
+  getTokenAtPosition,
+  getTypeInfo,
+} from 'graphql-language-service';
+import { FragmentDefinitionNode, GraphQLSchema, Kind, parse } from 'graphql';
+
+import { findNode } from './ast';
+import { getSource, getSuggestionsForFragmentSpread } from './utils';
+import { Cursor } from './ast/cursor';
+import { resolveTemplate } from './resolve';
+import { getToken } from './token';
+
+export function getGraphQLCompletions(
+  filename: string,
+  cursorPosition: number,
+  schema: { current: GraphQLSchema | null },
+  info: ts.server.PluginCreateInfo
+): ts.WithMetadata<ts.CompletionInfo> | undefined {
+  const tagTemplate = info.config.template || 'gql';
+
+  const source = getSource(info, filename);
+  if (!source) return undefined;
+
+  let node = findNode(source, cursorPosition);
+  if (!node) return undefined;
+
+  while (
+    isNoSubstitutionTemplateLiteral(node) ||
+    isToken(node) ||
+    isTemplateExpression(node)
+  ) {
+    node = node.parent;
+  }
+
+  if (isTaggedTemplateExpression(node)) {
+    const { template, tag } = node;
+    if (!isIdentifier(tag) || tag.text !== tagTemplate) return undefined;
+
+    const text = resolveTemplate(node, filename, info);
+    const foundToken = getToken(template, cursorPosition);
+
+    if (!foundToken || !schema.current) return undefined;
+
+    let fragments: Array<FragmentDefinitionNode> = [];
+    try {
+      const parsed = parse(text);
+      fragments = parsed.definitions.filter(
+        x => x.kind === Kind.FRAGMENT_DEFINITION
+      ) as Array<FragmentDefinitionNode>;
+    } catch (e) {}
+
+    const cursor = new Cursor(foundToken.line, foundToken.start);
+    const suggestions = getAutocompleteSuggestions(
+      schema.current,
+      text,
+      cursor
+    );
+
+    const token = getTokenAtPosition(text, cursor);
+    const spreadSuggestions = getSuggestionsForFragmentSpread(
+      token,
+      getTypeInfo(schema.current, token.state),
+      schema.current,
+      text,
+      fragments
+    );
+
+    const result: ts.WithMetadata<ts.CompletionInfo> = {
+      isGlobalCompletion: false,
+      isMemberCompletion: false,
+      isNewIdentifierLocation: false,
+      entries: [
+        ...suggestions.map(suggestion => ({
+          ...suggestion,
+          kind: ScriptElementKind.variableElement,
+          name: suggestion.label,
+          kindModifiers: 'declare',
+          sortText: suggestion.sortText || '0',
+          labelDetails: {
+            detail: suggestion.type
+              ? ' ' + suggestion.type?.toString()
+              : undefined,
+            description: suggestion.documentation,
+          },
+        })),
+        ...spreadSuggestions.map(suggestion => ({
+          ...suggestion,
+          kind: ScriptElementKind.variableElement,
+          name: suggestion.label,
+          insertText: '...' + suggestion.label,
+          kindModifiers: 'declare',
+          sortText: '0',
+          labelDetails: {
+            description: suggestion.documentation,
+          },
+        })),
+      ],
+    };
+    return result;
+  } else {
+    return undefined;
+  }
+}
--- packages/graphqlsp/src/index.ts
+++ packages/graphqlsp/src/index.ts
@@ -27,7 +27,7 @@ import {
 } from 'graphql';
 
 import { findAllImports, findAllTaggedTemplateNodes, findNode } from './ast';
-import { Cursor } from './cursor';
+import { Cursor } from './ast/cursor';
 import { loadSchema } from './getSchema';
 import { getToken } from './token';
 import {
@@ -37,6 +37,7 @@ import {
 } from './utils';
 import { resolveTemplate } from './resolve';
 import { generateTypedDocumentNodes } from './types/generate';
+import { getGraphQLCompletions } from './autoComplete';
 
 function createBasicDecorator(info: ts.server.PluginCreateInfo) {
   const proxy: ts.LanguageService = Object.create(null);
@@ -411,87 +412,18 @@ function create(info: ts.server.PluginCreateInfo) {
       entries: [],
     };
 
-    const source = getSource(info, filename);
-    if (!source) return originalCompletions;
+    const completions = getGraphQLCompletions(
+      filename,
+      cursorPosition,
+      schema,
+      info
+    );
 
-    let node = findNode(source, cursorPosition);
-    if (!node) return originalCompletions;
-
-    while (
-      isNoSubstitutionTemplateLiteral(node) ||
-      isToken(node) ||
-      isTemplateExpression(node)
-    ) {
-      node = node.parent;
-    }
-
-    if (isTaggedTemplateExpression(node)) {
-      const { template, tag } = node;
-      if (!isIdentifier(tag) || tag.text !== tagTemplate)
-        return originalCompletions;
-
-      const text = resolveTemplate(node, filename, info);
-      const foundToken = getToken(template, cursorPosition);
-
-      if (!foundToken || !schema.current) return originalCompletions;
-
-      let fragments: Array<FragmentDefinitionNode> = [];
-      try {
-        const parsed = parse(text);
-        fragments = parsed.definitions.filter(
-          x => x.kind === Kind.FRAGMENT_DEFINITION
-        ) as Array<FragmentDefinitionNode>;
-      } catch (e) {}
-
-      const cursor = new Cursor(foundToken.line, foundToken.start);
-      const suggestions = getAutocompleteSuggestions(
-        schema.current,
-        text,
-        cursor
-      );
-
-      const token = getTokenAtPosition(text, cursor);
-      const spreadSuggestions = getSuggestionsForFragmentSpread(
-        token,
-        getTypeInfo(schema.current, token.state),
-        schema.current,
-        text,
-        fragments
-      );
-
-      const result: ts.WithMetadata<ts.CompletionInfo> = {
-        isGlobalCompletion: false,
-        isMemberCompletion: false,
-        isNewIdentifierLocation: false,
-        entries: [
-          ...suggestions.map(suggestion => ({
-            ...suggestion,
-            kind: ScriptElementKind.variableElement,
-            name: suggestion.label,
-            kindModifiers: 'declare',
-            sortText: suggestion.sortText || '0',
-            labelDetails: {
-              detail: suggestion.type
-                ? ' ' + suggestion.type?.toString()
-                : undefined,
-              description: suggestion.documentation,
-            },
-          })),
-          ...spreadSuggestions.map(suggestion => ({
-            ...suggestion,
-            kind: ScriptElementKind.variableElement,
-            name: suggestion.label,
-            insertText: '...' + suggestion.label,
-            kindModifiers: 'declare',
-            sortText: '0',
-            labelDetails: {
-              description: suggestion.documentation,
-            },
-          })),
-          ...originalCompletions.entries,
-        ],
+    if (completions) {
+      return {
+        ...completions,
+        entries: [...completions.entries, ...originalCompletions.entries],
       };
-      return result;
     } else {
       return originalCompletions;
     }

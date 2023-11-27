e084c203a8f4ae0f54bafaaae6f712890cd64f64
Jovi De Croock <decroockjovi@gmail.com>
2023-11-27 10:38:53 +0100

    poll schema every minute

--- packages/graphqlsp/src/graphql/getSchema.ts
+++ packages/graphqlsp/src/graphql/getSchema.ts
@@ -31,24 +31,23 @@ export const loadSchema = (
     version: 0,
   };
   let url: URL | undefined;
-
-  let isJSON = false;
-  let config: undefined | SchemaOrigin;
+  let config: { headers: Record<string, unknown> } | undefined;
 
   try {
     if (typeof schema === 'object') {
       url = new URL(schema.url);
+      config = { headers: schema.headers };
     } else {
       url = new URL(schema);
     }
   } catch (e) {}
 
   if (url) {
-    logger(`Fetching introspection from ${url.toString()}`);
-    fetch(url.toString(), {
-      method: 'POST',
-      headers:
-        isJSON && config
+    const pollSchema = () => {
+      logger(`Fetching introspection from ${url!.toString()}`);
+      fetch(url!.toString(), {
+        method: 'POST',
+        headers: config
           ? {
               ...(config.headers || {}),
               'Content-Type': 'application/json',
@@ -56,46 +55,51 @@ export const loadSchema = (
           : {
               'Content-Type': 'application/json',
             },
-      body: JSON.stringify({
-        query: getIntrospectionQuery({
-          descriptions: true,
-          schemaDescription: false,
-          inputValueDeprecation: false,
-          directiveIsRepeatable: false,
-          specifiedByUrl: false,
+        body: JSON.stringify({
+          query: getIntrospectionQuery({
+            descriptions: true,
+            schemaDescription: false,
+            inputValueDeprecation: false,
+            directiveIsRepeatable: false,
+            specifiedByUrl: false,
+          }),
         }),
-      }),
-    })
-      .then(response => {
-        logger(`Got response ${response.statusText} ${response.status}`);
-        if (response.ok) return response.json();
-        else return response.text();
       })
-      .then(result => {
-        logger(`Got result ${JSON.stringify(result)}`);
-        if (typeof result === 'string') {
-          logger(`Got error while fetching introspection ${result}`);
-        } else if (result.data) {
-          try {
-            ref.current = buildClientSchema(
-              (result as { data: IntrospectionQuery }).data
-            );
-            ref.version = ref.version + 1;
-            logger(`Got schema for ${url!.toString()}`);
-            if (shouldTypegen)
-              generateBaseTypes(
-                ref.current,
-                baseTypesPath,
-                scalars,
-                extraTypes
+        .then(response => {
+          logger(`Got response ${response.statusText} ${response.status}`);
+          if (response.ok) return response.json();
+          else return response.text();
+        })
+        .then(result => {
+          logger(`Got result ${JSON.stringify(result)}`);
+          if (typeof result === 'string') {
+            logger(`Got error while fetching introspection ${result}`);
+          } else if (result.data) {
+            try {
+              ref.current = buildClientSchema(
+                (result as { data: IntrospectionQuery }).data
               );
-          } catch (e: any) {
-            logger(`Got schema error for ${e.message}`);
+              ref.version = ref.version + 1;
+              logger(`Got schema for ${url!.toString()}`);
+              if (shouldTypegen)
+                generateBaseTypes(
+                  ref.current,
+                  baseTypesPath,
+                  scalars,
+                  extraTypes
+                );
+            } catch (e: any) {
+              logger(`Got schema error for ${e.message}`);
+            }
+          } else {
+            logger(`Got invalid response ${JSON.stringify(result)}`);
           }
-        } else {
-          logger(`Got invalid response ${JSON.stringify(result)}`);
-        }
-      });
+        });
+    };
+
+    setInterval(() => {
+      pollSchema();
+    }, 1000 * 60);
   } else if (typeof schema === 'string') {
     const isJson = schema.endsWith('json');
     const resolvedPath = path.resolve(path.dirname(root), schema);

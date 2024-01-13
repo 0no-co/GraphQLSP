c1ee9610f6d89e7ce63711ae78779b36eed42d34
Phil Pluckthun <phil@kitten.sh>
2024-01-13 14:32:16 +0000

    Fix default-cjs output

--- scripts/rollup.config.mjs
+++ scripts/rollup.config.mjs
@@ -84,7 +84,7 @@ const commonPlugins = [
 
 const commonOutput = {
   dir: './',
-  exports: 'named',
+  exports: 'auto',
   sourcemap: true,
   sourcemapExcludeSources: false,
   hoistTransitiveImports: false,

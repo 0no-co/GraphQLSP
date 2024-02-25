8cf0a242407ea809d8a8f504349d42419961d478
Jovi De Croock <decroockjovi@gmail.com>
2024-02-25 10:20:48 +0100

    add tada test suite

---
+++ test/e2e/fixture-project-tada/fixtures/simple.ts
@@ -0,0 +1,16 @@
+import { graphql } from './graphql';
+import { PokemonFields } from './fragment';
+
+// prettier-ignore
+const x = graphql(`
+  query Pok($limit: Int!) {
+    pokemons(limit: $limit) {
+      id
+      name
+      fleeRate
+      classification
+      ...pokemonFields
+      __typename
+    }
+  }
+`, [PokemonFields]);

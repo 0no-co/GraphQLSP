248a3e76fe890dfec6885c2bb6d7c4beff6f6a07
Jovi De Croock <decroockjovi@gmail.com>
2024-04-05 13:48:27 +0200

    expose helper to translate typeQuery to document

--- packages/graphqlsp/src/api.ts
+++ packages/graphqlsp/src/api.ts
@@ -1,3 +1,4 @@
 export { getGraphQLDiagnostics } from './diagnostics';
 export { init } from './ts';
 export { findAllPersistedCallExpressions } from './ast';
+export { getDocumentReferenceFromTypeQuery } from './persisted';
